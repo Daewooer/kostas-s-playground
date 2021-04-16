0e454f2e456e39d2e85c814b6d03450c33639f41
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-04-16 11:14:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello")
+input()
+
+if input=="hi":
+    print("What is up")
+
+elif input=="hi :(":
+    print("Are you ok?")
+
+elif input=="yes":
+    print("Oh that is good")
+
+elif input=="no":
+    print("Everything will be alright, don't worry")
+
+elif input=="good":
+    print("Have a nice day :D")
+    
